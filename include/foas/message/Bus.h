89b9071c98f1317c7f5db7b4054c0806902f6bdd
FantasticInABun <fantastic.in.a.bun@gmail.com>
2018-03-11 23:00:58 +0100

    Implemented first functionality for the message bus

--- include/foas/message/Bus.h
+++ include/foas/message/Bus.h
@@ -2,12 +2,44 @@
 #define __FOAS_MESSAGE_BUS_H__
 
 
+#include <mutex>
+#include <condition_variable>
+#include <map>
+#include <list>
+#include <string>
+#include <memory>
+
+#include <foas/message/Message.h>
+
+
 namespace foas {
   namespace message {
-    class Bus {
+    class Bus : public std::enable_shared_from_this<Bus> {
+    private:
+      std::string mName;
+      
+      std::mutex mHasQueuedMessagesAvailableMutex;
+      bool mHasQueuedMessagesAvailable;
+      
+      std::mutex mQueuedMessagesMutex;
+      std::map<std::string, std::list<std::shared_ptr<Message>>> mQueuedMessages;
+
+      std::shared_ptr<Bus> mParentBus;
+      std::mutex mSubBussesMutex;
+      std::map<std::string, std::shared_ptr<Bus>> mSubBusses;
+
     public:
-      Bus();
+      Bus(std::string name, std::shared_ptr<Bus> parentBus = nullptr);
       ~Bus();
+      
+      bool HasQueuedMessagesAvailable();
+      void QueueMessage(std::string topic, std::shared_ptr<Message> message);
+      std::map<std::string, std::list<std::shared_ptr<Message>>> CollectQueuedMessages();
+      
+      void PostMessage(std::string topic, std::shared_ptr<Message> message);
+      
+      std::shared_ptr<Bus> CreateSubBus(std::string name);
+      void RemoveSubBus(std::string name);
     };
   }
 }
--- src/message/Bus.cpp
+++ src/message/Bus.cpp
@@ -3,10 +3,56 @@
 
 namespace foas {
   namespace message {
-    Bus::Bus() {
+    Bus::Bus(std::string name, std::shared_ptr<Bus> parentBus) : mName(name), mParentBus(parentBus) {
     }
 
     Bus::~Bus() {
+      if(mParentBus) {
+	mParentBus->RemoveSubBus(mName);
+      }
+    }
+
+    bool Bus::HasQueuedMessagesAvailable() {
+      std::lock_guard<std::mutex> lock(mHasQueuedMessagesAvailableMutex);
+      return mHasQueuedMessagesAvailable;
+    }
+    
+    void Bus::QueueMessage(std::string topic, std::shared_ptr<Message> message) {
+      std::lock_guard<std::mutex> hasLock(mHasQueuedMessagesAvailableMutex);
+      mHasQueuedMessagesAvailable = true;
+      
+      std::lock_guard<std::mutex> queueLock(mQueuedMessagesMutex);
+      mQueuedMessages[topic].push_back(message);
+    }
+    
+    std::map<std::string, std::list<std::shared_ptr<Message>>> Bus::CollectQueuedMessages() {
+      std::lock_guard<std::mutex> hasLock(mHasQueuedMessagesAvailableMutex);
+      std::lock_guard<std::mutex> queueLock(mQueuedMessagesMutex);
+      
+      std::map<std::string, std::list<std::shared_ptr<Message>>> queuedMessages = mQueuedMessages;
+      mQueuedMessages.clear();
+      mHasQueuedMessagesAvailable = false;
+      
+      return queuedMessages;
+    }
+    
+    void Bus::PostMessage(std::string topic, std::shared_ptr<Message> message) {
+      // TODO: Iterate through all participants that are interested in 'topic' and sent them the 'message'.
+    }
+    
+    std::shared_ptr<Bus> Bus::CreateSubBus(std::string name) {
+      std::lock_guard<std::mutex> lockSubBusses(mSubBussesMutex);
+      
+      std::shared_ptr<Bus> subBus = std::make_shared<Bus>(name, this->shared_from_this());
+      mSubBusses[name] = subBus;
+      
+      return subBus;
+    }
+    
+    void Bus::RemoveSubBus(std::string name) {
+      std::lock_guard<std::mutex> lockSubBusses(mSubBussesMutex);
+      
+      mSubBusses.erase(mSubBusses.find(name));
     }
   }
 }
